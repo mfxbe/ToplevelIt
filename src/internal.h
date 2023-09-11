c4f9f8f9768aa5325b42a042ef0fd3b578aa8d03
Manuel Ehrmanntraut <mfxbe@feritale.eu>
2023-09-11 13:26:23 +0200

    Code style fix

--- src/internal.h
+++ src/internal.h
@@ -3,7 +3,9 @@
 
 #include "toplevelit_manager.h"
 #include "foreign-toplevel.h"
+
 extern int lastWinIDCounter;
+
 void toplevel_manager_start(ToplevelItManager *);
 
 void internal_set_state(struct zwlr_foreign_toplevel_handle_v1 *, int);
